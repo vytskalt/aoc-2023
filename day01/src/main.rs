b442ba3dc359bef456c232f1995293f741fc6884
vytskalt <vytskalt@protonmail.com>
2023-12-01 18:19:47 +0200

    day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,49 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").unwrap();
+    let result = input.lines().map(|line| {
+        let (first, last) = extract_first_last(line).unwrap();
+        10 * first + last
+    }).sum::<u32>();
+    println!("{}", result);
+}
+
+fn extract_first_last(line: &str) -> Option<(u32, u32)> {
+    let mut digits = vec![];
+
+    for (i, c) in line.chars().enumerate() {
+        if let Some(n) = c.to_digit(10) {
+            digits.push(n);
+            continue;
+        }
+
+        if let Some(n) = extract_from_end(&line[..i + 1]) {
+            digits.push(n);
+        }
+    }
+
+    Some((*digits.first()?, *digits.last()?))
+}
+
+fn extract_from_end(input: &str) -> Option<u32> {
+    let digits = [
+        ("nine", 9),
+        ("eight", 8),
+        ("seven", 7),
+        ("six", 6),
+        ("five", 5),
+        ("four", 4),
+        ("three", 3),
+        ("two", 2),
+        ("one", 1)
+    ];
+
+    for (digit, value) in digits {
+        if input.ends_with(digit) {
+            return Some(value);
+        }
+    }
+
+    None
 }
