2bd5fd04d814198d12296f4a0eb66827a7ddca1a
vytskalt <vytskalt@protonmail.com>
2023-12-09 13:54:08 +0200

    day 9

---
+++ day09/src/bin/part2.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = process(input).unwrap();
+    println!("{}", output);
+}
+
+fn process(input: &str) -> anyhow::Result<i32> {
+    let sum = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse::<i32>())
+                .collect::<Result<_, _>>()
+        })
+        .map(|line| line.map(|line| process_line(line)))
+        .map(|nums| {
+            nums.map(|nums| {
+                nums.iter()
+                    .rev()
+                    .map(|x| x.first().unwrap())
+                    .fold(0, |acc, x| x - acc)
+            })
+        })
+        .sum::<Result<i32, _>>()?;
+    Ok(sum)
+}
+
+fn process_line(line: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut out = vec![line];
+    loop {
+        let last = out.last().unwrap();
+        let gaming = last
+            .iter()
+            .zip(last.iter().skip(1))
+            .map(|(a, b)| *b - *a)
+            .collect::<Vec<_>>();
+
+        let should_end = gaming.iter().all(|a| *a == 0);
+        out.push(gaming);
+        if should_end {
+            break out;
+        }
+    }
+}
+
