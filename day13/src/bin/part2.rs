9e4e325318e0ab48ea4a03e0cb140df37d972f57
vytskalt <vytskalt@protonmail.com>
2023-12-13 18:46:01 +0200

    day 13

---
+++ day13/src/bin/part2.rs
@@ -0,0 +1,151 @@
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+enum Tile {
+    Ash,
+    Rocks,
+}
+
+impl Tile {
+    fn from_char(char: char) -> Self {
+        match char {
+            '.' => Self::Ash,
+            '#' => Self::Rocks,
+            _ => panic!("Invalid tile char '{char}'"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Pattern {
+    width: usize,
+    height: usize,
+    tiles: Vec<Tile>,
+}
+
+impl Pattern {
+    fn parse(part: &str) -> Self {
+        let tiles = part
+            .lines()
+            .flat_map(|line| line.chars().map(Tile::from_char))
+            .collect::<Vec<_>>();
+        let width = part.lines().next().unwrap().len();
+        let height = tiles.len() / width;
+
+        Self {
+            width,
+            height,
+            tiles,
+        }
+    }
+
+    fn get(&self, x: isize, y: isize) -> Option<Tile> {
+        if x < 0 || y < 0 {
+            return None;
+        }
+
+        let (x, y) = (x as usize, y as usize);
+        if x >= self.width || y >= self.height {
+            return None;
+        }
+
+        self.tiles.get(y * self.width + x).copied()
+    }
+
+    fn row_matches(&self, smudge_found: &mut bool, y1: isize, y2: isize) -> Option<bool> {
+        let mut diff_count = 0;
+        for x in 0..self.width as isize {
+            let tile1 = self.get(x, y1)?;
+            let tile2 = self.get(x, y2)?;
+            if tile1 != tile2 {
+                diff_count += 1;
+            }
+        }
+        Some(self.check_match(diff_count, smudge_found))
+    }
+
+    fn col_matches(&self, smudge_found: &mut bool, x1: isize, x2: isize) -> Option<bool> {
+        let mut diff_count = 0;
+        for y in 0..self.height as isize {
+            let tile1 = self.get(x1, y)?;
+            let tile2 = self.get(x2, y)?;
+            if tile1 != tile2 {
+                diff_count += 1;
+            }
+        }
+        Some(self.check_match(diff_count, smudge_found))
+    }
+
+    fn check_match(&self, diff_count: u32, smudge_found: &mut bool) -> bool {
+        if diff_count == 0 {
+            return true;
+        }
+
+        if diff_count == 1 && !*smudge_found {
+            *smudge_found = true;
+            return true;
+        }
+
+        false
+    }
+
+    fn calc_reflections(&self) -> u32 {
+        'outer: for x in 0..self.width as isize {
+            let mut smudge_used = false;
+            let matches = self.col_matches(&mut smudge_used, x, x + 1);
+            if !matches.unwrap_or(false) {
+                continue;
+            }
+
+            for i in 1.. {
+                let x1 = x - i;
+                let x2 = x + i + 1;
+                match self.col_matches(&mut smudge_used, x1, x2) {
+                    Some(true) => continue,
+                    Some(false) => continue 'outer,
+                    None => break,
+                }
+            }
+
+            if smudge_used {
+                return x as u32 + 1;
+            }
+        }
+
+        'outer: for y in 0..self.height as isize {
+            let mut smudge_used = false;
+            let matches = self.row_matches(&mut smudge_used, y, y + 1);
+            if !matches.unwrap_or(false) {
+                continue;
+            }
+
+            for i in 1.. {
+                let y1 = y - i;
+                let y2 = y + i + 1;
+                match self.row_matches(&mut smudge_used, y1, y2) {
+                    Some(true) => continue,
+                    Some(false) => continue 'outer,
+                    None => break,
+                }
+            }
+
+            if smudge_used {
+                return (y as u32 + 1) * 100;
+            } 
+        }
+
+        panic!("No reflection found");
+    }
+}
+
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = process(input);
+    println!("{}", output);
+}
+
+fn process(input: &str) -> u32 {
+    input
+        .split("\n\n")
+        .map(Pattern::parse)
+        .map(|pattern| pattern.calc_reflections())
+        .sum::<u32>()
+}
